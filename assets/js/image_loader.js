add13e59afb918c731641cf81a22678729ce6776
JiwooPark <jpark830@me.com>
2018-07-04 18:38:22 -0700

    add google analytics

--- assets/js/image_loader.js
+++ assets/js/image_loader.js
@@ -1,3 +1,12 @@
+const imageData = {
+  "001" : "one",
+  "002" : "two",
+  "003" : "three",
+  "004" : "four",
+  "005" : "five",
+  "006" : "six",
+}
+
 $(document).ready(function(){
   const totalNumOfImage = 13
   const mediaElement = $('.medias')
@@ -10,6 +19,7 @@ $(document).ready(function(){
         <div class="media">
           <div class="inner-content">
             <img class="fade-in" src="https://s3-us-west-1.amazonaws.com/james-web-content/test/images/flowers/${padCounter}.jpg" style="display: block;">
+            <span>${imageData[padCounter] === undefined ? "" : imageData[padCounter]}</span>
           </div>
         </div>
       `
@@ -26,5 +36,8 @@ $(document).ready(function(){
       });
     }
   });
+
+  
 })
 
+console.log(imageData)
